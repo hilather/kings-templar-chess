5782a7e471e8aff2afff55eb7836e464de47c9c8
hilather <hilather@gmail.com>
2026-08-06 12:00:15 -0400

    feat: RTC signaling server, engine tests, useP2PRoom, npm test script

--- src/lib/multiplayer/index.ts
+++ src/lib/multiplayer/index.ts
@@ -7,3 +7,14 @@ export type {
   SignalRow,
   RtcPollResponse,
 } from "./p2p";
+export { useP2PRoom } from "./use-p2p-room";
+export type { UseP2PRoomOptions, P2PRoomHandle } from "./use-p2p-room";
+export {
+  normalizeRoomId,
+  generateRoomId,
+  isValidRoomId,
+  shareRoomUrl,
+  historyToWire,
+  isChessNetMessage,
+} from "./protocol";
+export type { WireMove, ChessNetMessage } from "./protocol";
